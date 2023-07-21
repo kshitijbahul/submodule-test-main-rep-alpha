eee4818481f26ba0ea02a458db0a65141a625a3d
kshitijbahul <chunks.3@gmail.com>
2023-07-21 14:29:36 +0000

    Added basic node code

---
+++ index.js
@@ -0,0 +1,10 @@
+const { printMessgae } = require('library-folder-for-submodule-test/utils')
+
+
+
+
+const handlerFunc = () => {
+    printMessgae({msg: "This is the first in the code"});
+}
+
+handlerFunc();
